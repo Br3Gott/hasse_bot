aeea5e7db9082b905746660a5f277996d450a9b3
Br3Gott <dawesohl@gmail.com>
2021-01-22 14:49:37 +0100

    New features and bugfixes

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-//fixa youtube sök
-//hasse decieds
+/*---------------------------------------------------------
+TO DO:
+*Youtube request queue
+    *Request playlist
+*Youtube pause/unpause function
 
+---------------------------------------------------------*/
 //Discord init
 const Discord = require('discord.js');
 const client = new Discord.Client();
@@ -21,6 +25,13 @@ const say = require('say');
 //Youtube downloader init
 const ytdl = require('ytdl-core');
 
+//Youtube search init
+const ytsr = require('ytsr');
+
+//App Variables
+var playing = false;
+var kilo = 1;
+
 //App is ready
 client.on('ready', () => {
     console.log(`App started! Connected as BOT:[${client.user.tag}]`);
@@ -29,6 +40,34 @@ client.on('ready', () => {
 client.on('message', async message => {
     // Check if message is from guild(server) otherwise return;
     if (!message.guild) return;
+
+    if (message.content.includes("1kg mjöl") || message.content.includes("1kilo mjöl") || message.content.includes("1kgmjöl") || message.content.includes("1kilomjöl") || message.content.includes("1kg") || message.content.includes("1kilo") || message.content.includes("mjöl")) {
+        // Only try to join the sender's voice channel if they are in one themselves
+        if (message.member.voice.channel) {
+            if (!playing) {
+                playing = true;
+                
+                //For random sound clip
+                kilo = Math.floor(Math.random()*3)+5;
+
+                const connection = await message.member.voice.channel.join();
+                const dispatcher = connection.play('./src/kilo/' + kilo + '.mp3');
+                dispatcher.setVolume(0.1);
+                dispatcher.on('finish', () => {
+                    kilo++
+                    if(kilo > 8){
+                        message.reply('Mjölet är nu slut!'); 
+                        kilo = 1;
+                    }
+                    console.log('Finished playing and leaving voice');
+                    message.member.voice.channel.leave();
+                    playing = false;
+                });
+            }
+        } else {
+            message.reply('Vart fan vill du att jag ska då?');
+        }
+    }
 
     //Check if message starts with configured prefix and is not from bot.
     if (!message.content.startsWith(prefix) || message.author.bot) return;
@@ -50,6 +89,10 @@ client.on('message', async message => {
 
             var diff = times[message.author.id].out - times[message.author.id].in;
 
+            var t_s = time_convert(diff);
+
+            console.log(message.author.username + ": Uppdaterat räkning. Current: " + t_s.h + "h " + t_s.m + "m " + t_s.s + "s.");
+
             var currentTime = db.get("users").find({
                 id: message.author.id
             }).value();
@@ -58,7 +101,7 @@ client.on('message', async message => {
                 currDiff = diff;
                 db.get("users").push({
                     id: message.author.id,
-                    name: message.author,
+                    name: message.author.username,
                     time: diff
                 }).write();
             } else {
@@ -73,6 +116,7 @@ client.on('message', async message => {
             //Start counting again
             time.in = Date.now();
             times[message.author.id] = time;
+
         }  
 
         var currentTime = db.get("users").find({
@@ -96,7 +140,7 @@ client.on('message', async message => {
 
     if (command === "stats") {
 
-        //FIX: update db before print
+        //FIX: update db before print ?loopa genom times variable...
 
         var elementNum = 1;
 
@@ -120,7 +164,7 @@ client.on('message', async message => {
 
             var t_s = time_convert(element.time);
 
-            if (elementNum < 4) {
+            if (elementNum < 6) {
                 embed.fields.push({
                     "name": "#" + elementNum,
                     "value": element.name + ", **" + t_s.h + "h " + t_s.m + "m " + t_s.s + "s**"
@@ -141,16 +185,43 @@ client.on('message', async message => {
         });
     }
 
+    if (command === "pick") {
+        // Only try to pick if the the sender is in a voice channel
+        if (message.member.voice.channel) {
+
+            var channelMembers = message.member.voice.channel.members;
+            var users = "";
+            var usersArr = [];
+            var count = 0;
+            channelMembers.forEach(member => {
+                users += member.user.username + ", ";
+                count++;
+                usersArr[count] = member.user.username;
+            });
+            message.channel.send("Mellan dessa idioter: " + users);
+
+            var randomNum = Math.floor((Math.random()* count)+1);
+            message.channel.send("Så väljer jag: " + usersArr[randomNum]);
+
+        } else {
+            message.reply('Vart fan vill du att jag ska kolla då?');
+        }
+    }
+
     if (command === "join") {
         // Only try to join the sender's voice channel if they are in one themselves
         if (message.member.voice.channel) {
-            const connection = await message.member.voice.channel.join();
-            const dispatcher = connection.play('./music.mp3');
-            dispatcher.setVolume(0.1);
-            dispatcher.on('finish', () => {
-                console.log('Finished playing and leaving voice');
-                message.member.voice.channel.leave();
-            });
+            if (!playing) {
+                playing = true;
+                const connection = await message.member.voice.channel.join();
+                const dispatcher = connection.play('./music.mp3');
+                dispatcher.setVolume(0.1);
+                dispatcher.on('finish', () => {
+                    console.log('Finished playing and leaving voice');
+                    message.member.voice.channel.leave();
+                    playing = false;
+                });
+            }
         } else {
             message.reply('Vart fan vill du att jag ska då?');
         }
@@ -166,20 +237,23 @@ client.on('message', async message => {
 
         // Only try to join the sender's voice channel if they are in one themselves
         if (message.member.voice.channel) {
-
-            say.export(text, null, 1, './src/temp.wav', async (err) => {
-                if (err) {
-                    return console.error(err)
-                }
-                console.log('Text exported to temp.wav.')
-                const connection = await message.member.voice.channel.join();
-                const dispatcher = connection.play('./src/temp.wav');
-                dispatcher.setVolume(0.1);
-                dispatcher.on('finish', () => {
-                    console.log('Finished playing and leaving voice');
-                    message.member.voice.channel.leave();
-                });
-            })
+            if (!playing) {
+                playing = true;
+                say.export(text, null, 1, './src/temp.wav', async (err) => {
+                    if (err) {
+                        return console.error(err)
+                    }
+                    console.log('Text exported to temp.wav.')
+                    const connection = await message.member.voice.channel.join();
+                    const dispatcher = connection.play('./src/temp.wav');
+                    dispatcher.setVolume(0.1);
+                    dispatcher.on('finish', () => {
+                        console.log('Finished playing and leaving voice');
+                        message.member.voice.channel.leave();
+                        playing = false;
+                    });
+                })
+            }
         } else {
             message.reply('Vart fan vill du att jag ska då?');
         }
@@ -195,16 +269,48 @@ client.on('message', async message => {
 
         // Only try to join the sender's voice channel if they are in one themselves
         if (message.member.voice.channel) {
-            const connection = await message.member.voice.channel.join();
-            const dispatcher = connection.play(ytdl(text, {
-                filter: 'audioonly'
-            }));
-            dispatcher.setVolume(0.1);
-            dispatcher.on('finish', () => {
-                console.log('Finished playing and leaving voice');
-                message.member.voice.channel.leave();
-            });
+            if (!playing) {
+                playing = true;
+                const connection = await message.member.voice.channel.join();
+                const dispatcher = connection.play(ytdl(text, {
+                    filter: 'audioonly'
+                }));
+                dispatcher.setVolume(0.1);
+                dispatcher.on('finish', () => {
+                    console.log('Finished playing and leaving voice');
+                    message.member.voice.channel.leave();
+                    playing = false;
+                });
+            }
+        } else {
+            message.reply('Vart fan vill du att jag ska då?');
+        }
+    }
 
+    if (command === "search") {
+
+        if (!args.length) {
+            return message.channel.send(`No arguments, ${message.author}!`);
+        }
+
+        var text = message.content.replace(prefix + "search ", "");
+
+        // Only try to join the sender's voice channel if they are in one themselves
+        if (message.member.voice.channel) {
+            if (!playing) {
+                playing = true;
+                const connection = await message.member.voice.channel.join();
+                const res = await ytsr(text, {limit: 1})
+                const dispatcher = connection.play(ytdl(res.items[0].url, {
+                    filter: 'audioonly'
+                }));
+                dispatcher.setVolume(0.1);
+                dispatcher.on('finish', () => {
+                    console.log('Finished playing and leaving voice');
+                    message.member.voice.channel.leave();
+                    playing = false;
+                });
+            }
         } else {
             message.reply('Vart fan vill du att jag ska då?');
         }
@@ -212,6 +318,7 @@ client.on('message', async message => {
 
     if (command === "stop") {
         message.member.voice.channel.leave();
+        playing = false;
     }
 
 });
@@ -291,13 +398,17 @@ client.on('voiceStateUpdate', async function (data, newdata) {
         if (newdata.selfMute == true) {
 
             if (data.member.voice.channel) {
-                const connection = await data.member.voice.channel.join();
-                const dispatcher = connection.play('./src/' + (Math.floor(Math.random() * 7) + 1) + '.mp3');
-                dispatcher.setVolume(0.1);
-                dispatcher.on('finish', () => {
-                    console.log('Finished playing and leaving voice');
-                    data.member.voice.channel.leave();
-                });
+                if (!playing) {
+                    playing = true;
+                    const connection = await data.member.voice.channel.join();
+                    const dispatcher = connection.play('./src/' + (Math.floor(Math.random() * 7) + 1) + '.mp3');
+                    dispatcher.setVolume(0.1);
+                    dispatcher.on('finish', () => {
+                        console.log('Finished playing and leaving voice');
+                        data.member.voice.channel.leave();
+                        playing = false;
+                    });
+                }
             } 
         }
 
